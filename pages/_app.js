cd90f619f2e02d0fed6b8c49e08af8ab32e1056b
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-16 03:43:41 +0500

    v3

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import "../styles/sass/application.scss";
 
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-2RCR6WE1BW' });
+  }, []);
   return <Component {...pageProps} />;
 }
