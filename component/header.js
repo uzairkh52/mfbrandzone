96e3faf199d45ea3c4d8431d3e238621eb821a96
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-16 07:44:16 +0500

    v2

--- component/header.js
+++ component/header.js
@@ -11,32 +11,21 @@ const Header = () => {
 <meta name="google-site-verification" content="l556Qqvxm0WIu4TDeOY0EMYPtEAd6zYR0nS9JBMNWoo" />
   {/* <!-- Google tag (gtag.js) --> */}
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-2RCR6WE1BW"></script>
+  {/* <!-- Google tag (gtag.js) --> */}
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WJEKHDZLXH"></script>
 <script>
   {`window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-2RCR6WE1BW');`}
+  gtag('config', 'G-WJEKHDZLXH');`}
 </script>
 
-  {/* google tag manager */}
-  {/* <!-- Google Tag Manager --> */}
-  <Script>
-  {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-2RCR6WE1BW');`}</Script>
-  {/* <!-- End Google Tag Manager --> */}
 </Head>
 {/* <!-- Google Tag Manager (noscript) --> */}
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-2RCR6WE1BW"
 height="0" width="0" className={styles.dNone + styles.hidden}></iframe></noscript>
 
-{/* <!-- End Google Tag Manager (noscript) --> */}
-
-      {/* <Image src="../public/images/favicon.svg" alt="Vercel Logo" /> */}
     </>
   );
 };
--- pages/_document.js
+++ pages/_document.js
@@ -5,29 +5,9 @@ export default function Document() {
     <Html lang="en">
       {/* <Head /> */}
       <Head>
-          {/* Google Tag Manager Script */}
-          <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','G-2RCR6WE1BW');
-            `,
-          }}
-        />
+          
       </Head>
       <body>
-         {/* Google Tag Manager NoScript */}
-         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=G-2RCR6WE1BW"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <Main />
         
         <NextScript />
