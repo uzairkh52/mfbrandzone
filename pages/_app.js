85d4e205131256bb77ba3b6ca7c30b791781c624
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-16 06:26:44 +0500

    v8

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import "@/styles/globals.css";
 import "../styles/sass/application.scss";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
     return <>
-    <GoogleAnalytics trackPageViews />
     <Component {...pageProps} />;
 
   </>
