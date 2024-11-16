3a256e80316647236cb02e6d3f6ed35111d696f5
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-16 05:59:43 +0500

    v5

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import "../styles/sass/application.scss";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
+    useEffect(() => {
+      ReactGA.initialize('Your-GA-Measurement-ID');
+      ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
+  <GoogleAnalytics trackPageViews />
+
   return <Component {...pageProps} />;
 }
