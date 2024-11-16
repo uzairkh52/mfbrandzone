fcf3419c78e4efc0c6990cbec82e63157a37cca6
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-16 06:05:36 +0500

    v7

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,7 @@ import "../styles/sass/application.scss";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
-    useEffect(() => {
-      ReactGA.initialize('G-2RCR6WE1BW');
-      ReactGA.pageview(window.location.pathname + window.location.search);
-    }, []);
-    
     return <>
-    <GoogleAnalytics trackPageViews />
     <Component {...pageProps} />;
 
   </>
