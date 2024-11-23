938111b66f5262618c17399f5d8f8511c11f858e
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-23 07:33:07 +0500

    33

--- component/header.js
+++ component/header.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Script from 'next/script'
 import Head from 'next/head';
-import styles from "@/styles/sass/components/Header.module.scss"
+import styles from "../styles/sass/components/Header.module.scss"
+import { Container, Grid } from "@mui/material";
 
 const Header = () => {
   
   return (
     <>
 <Head>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="icon" href="/favicon.ico" />
 {/* <!-- Google tag (gtag.js) --> */}
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-JSWTFCCN6J"></script>
 <script>
@@ -23,6 +26,17 @@ const Header = () => {
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-JSWTFCCN6J"
 height="0" width="0" className={styles.dNone + styles.hidden}></iframe></noscript>
 
+<section className="white-bg">
+        <Container maxWidth="sm">
+          <Grid>
+            <Grid item xs={12}>
+            <div className="d-flex justify-content-center align-items-center">
+              <img style={{ width: 150 }} src="/images/logo.jpg" />
+            </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </section>
     </>
   );
 };
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 import Link from "next/link";
-import Header from "@/component/header";
+import Header from "../../component/header";
 
 export default function About() {
   return (
